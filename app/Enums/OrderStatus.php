859fa8ffb09ac2173e0877c9cc45e8b343504b8a
jjlabrada <jjlabrada@xetid.cu>
2026-06-23 19:01:28 -0400

    feat: add OrderStatus enum for order management

---
+++ app/Enums/OrderStatus.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Enums;
+
+enum OrderStatus: string
+{
+    case PENDING = 'pending';
+    case COMPLETED = 'completed';
+    case REFUNDED  = 'refunded';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::PENDING => 'Order in proccess',
+            self::COMPLETED => 'Completed order',
+            self::REFUNDED => 'Payment refunded',
+        };
+    }
+
+    public function color(): string
+    {
+        return match ($this) {
+            self::PENDING => 'warning',
+            self::COMPLETED => 'success',
+            self::REFUNDED => 'gray',
+        };
+    }
+}
