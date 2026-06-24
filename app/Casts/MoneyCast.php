488dfb79db01c59543fe546f71e4080173efb49a
jjlabrada <jjlabrada@xetid.cu>
2026-06-24 15:38:59 -0400

    fix: add validation to MoneyCast

--- app/Casts/MoneyCast.php
+++ app/Casts/MoneyCast.php
@@ -14,7 +14,7 @@ class MoneyCast implements CastsAttributes
      */
     public function get(Model $model, string $key, mixed $value, array $attributes): mixed
     {
-        return $value / 100;
+        return $value !== null ? $value / 100 : null;
     }
 
     /**
@@ -24,6 +24,6 @@ class MoneyCast implements CastsAttributes
      */
     public function set(Model $model, string $key, mixed $value, array $attributes): mixed
     {
-        return $value * 100;
+        return $value !== null ? $value * 100 : null;
     }
 }
