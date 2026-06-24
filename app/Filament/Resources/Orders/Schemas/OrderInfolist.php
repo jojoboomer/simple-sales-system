e3ecdf02811eb0183016cb896765e79c96b6cf97
jjlabrada <jjlabrada@xetid.cu>
2026-06-24 01:17:10 -0400

    feat: enhance OrderInfolist schema with improved components and layout

--- app/Filament/Resources/Orders/Schemas/OrderInfolist.php
+++ app/Filament/Resources/Orders/Schemas/OrderInfolist.php
@@ -2,7 +2,11 @@
 
 namespace App\Filament\Resources\Orders\Schemas;
 
+use App\Enums\OrderStatus;
+use Filament\Infolists\Components\RepeatableEntry;
+use Filament\Infolists\Components\RepeatableEntry\TableColumn;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class OrderInfolist
@@ -10,17 +14,37 @@ class OrderInfolist
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(3)
             ->components([
                 TextEntry::make('total')
-                    ->numeric(),
+                    ->money(),
+                TextEntry::make('status')->badge()->color(fn(OrderStatus $state) => $state->color()),
+                TextEntry::make('user.name')
+                    ->label('User'),
                 TextEntry::make('created_at')
                     ->dateTime()
                     ->placeholder('-'),
                 TextEntry::make('updated_at')
                     ->dateTime()
                     ->placeholder('-'),
-                TextEntry::make('user.name')
-                    ->label('User'),
+
+                RepeatableEntry::make('orderProducts')
+                    ->columnSpan(3)
+                    ->label('Products')
+                    ->table([
+                        TableColumn::make('Name'),
+                        TableColumn::make('Quantity'),
+                        TableColumn::make('Price'),
+                        TableColumn::make('Total'),
+                    ])
+                    ->columns(2)
+                    ->grid(2)
+                    ->schema([
+                        TextEntry::make('product.name')->label('Producto'),
+                        TextEntry::make('quantity')->numeric(),
+                        TextEntry::make('product_price')->money(),
+                        TextEntry::make('subtotal')->money()
+                    ])
             ]);
     }
 }
