0cd4b6bc6800ff0a4af292b37e4af98a32e0f1c4
jjlabrada <jjlabrada@xetid.cu>
2026-06-24 16:55:46 -0400

    refactor: enum label for pending status

--- app/Enums/OrderStatus.php
+++ app/Enums/OrderStatus.php
@@ -11,7 +11,7 @@ enum OrderStatus: string
     public function label(): string
     {
         return match ($this) {
-            self::PENDING => 'Order in proccess',
+            self::PENDING => 'Pending order',
             self::COMPLETED => 'Completed order',
             self::REFUNDED => 'Payment refunded',
         };
